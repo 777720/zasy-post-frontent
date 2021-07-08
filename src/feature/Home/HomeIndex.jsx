2c092e1236bdf7fe9cffaffe70845aae6d9d2d6b
geek720 <geek720s@gmail.com>
2021-07-08 18:19:52 +0800

    bug: some2

--- src/feature/Home/HomeIndex.jsx
+++ src/feature/Home/HomeIndex.jsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { useHistory } from 'react-router-dom'
 import './styles/index.scss'
+import {useState} from "react";
 
 
 export const HomeIndex = (props) => {
     let history = useHistory();
+
+    const [currentTab, setCurrentTab] = useState(1);
+
+    const onClickTabFn = (tabIndex) => {
+        setCurrentTab(tabIndex)
+    }
 
     const onClickAddThing = () => {
         history.push('/add')
@@ -12,11 +19,11 @@ export const HomeIndex = (props) => {
     return (
         <div className='pg-home'>
             <div className='bottom-btn'>
-                <div className="flex-item">
-                    <img className="bottom-btn--img" src={require('../../assets/images/daojishi.png')} />
+                <div className={ `flex-item${currentTab === 1 ? '--click' : ''}` } onClick={() => { onClickTabFn(1) }}>
+                    <img  src={require('../../assets/images/daojishi.png')} />
                 </div>
-                <div className="flex-item">
-                    <img className="bottom-btn--img" src={require('../../assets/images/daojishi.png')} />
+                <div className={ `flex-item${currentTab === 2 ? '--click' : ''}` } onClick={() => { onClickTabFn(2) }}>
+                    <img  src={require('../../assets/images/daojishi.png')} />
                 </div>
 
             </div>
