4da977be33a270a6010c0cc1d28658109a597bb8
geek720 <geek720s@gmail.com>
2021-07-09 17:32:19 +0800

    chore: none

---
+++ src/feature/Home/DayPlanCard.jsx
@@ -0,0 +1,13 @@
+import react from 'React';
+
+export const DayPlanCard = () => {
+    return (
+        <div className="card-day-plan">
+            <div className="day-item"></div>
+            <div className="day-item"></div>
+            <div className="day-item"></div>
+
+        </div>
+    )
+}
+
--- src/feature/Home/HomeIndex.jsx
+++ src/feature/Home/HomeIndex.jsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
+import  React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import './styles/index.scss'
-import {useState} from "react";
 
 
 export const HomeIndex = (props) => {
@@ -18,6 +17,10 @@ export const HomeIndex = (props) => {
     }
     return (
         <div className='pg-home'>
+            <div className="home-container">
+
+                123
+            </div>
             <div className='bottom-btn'>
                 <div className={ `flex-item${currentTab === 1 ? '--click' : ''}` } onClick={() => { onClickTabFn(1) }}>
                     <img  src={require('../../assets/images/daojishi.png')} />
@@ -25,7 +28,6 @@ export const HomeIndex = (props) => {
                 <div className={ `flex-item${currentTab === 2 ? '--click' : ''}` } onClick={() => { onClickTabFn(2) }}>
                     <img  src={require('../../assets/images/daojishi.png')} />
                 </div>
-
             </div>
         </div>
     );
