77477a37be436a723306009c4a7d218cf6622196
geek720 <geek720s@gmail.com>
2021-07-14 18:26:26 +0800

    fix: ui

--- src/feature/Home/DayPlanCard.jsx
+++ src/feature/Home/DayPlanCard.jsx
@@ -5,61 +5,71 @@ export const DayPlanCard = () => {
 
     const dragDiv = useRef()
 
-    let currentLeft = 0
-    const [startDivPosition, setStartDivPosition] = useState([]);
-
     const [movePosition, setMovePosition] = useState([0, 0]);
-    const [startPositionX, setStartPositionX] = useState(0)
+
+    const [moveStartPont, setMoveStartPont] = useState([0, 0]);
+    const [hasTransition, setHasTransition] = useState(false);
 
     const handleTouchStartFn = (evt) => {
         if(!evt.touches && evt.touches.length === 0) {
             return;
         }
         const { pageX, pageY } = evt.touches[0]
-        const { offsetLeft, offsetTop } = dragDiv.current;
-        setStartDivPosition([offsetLeft, offsetTop])
-        setStartPositionX(pageX);
+        setMoveStartPont([pageX, pageY]);
+    }
+
+    const  handleTouchEndFn = (evt) => {
+        setMovePosition([0, 0])
     }
     const  handleTouchMoveFn = (evt) => {
         evt.preventDefault();
         if(!evt.touches && evt.touches.length === 0) {
             return;
         }
-        const { pageX, pageY } = evt.touches[0]
-        const moveX = pageX - startPositionX;
+        const { pageX } = evt.touches[0]
+        const moveX = pageX - moveStartPont[0];
 
         if (Math.abs(moveX) > 0) {
+            setHasTransition(true);
             setMovePosition([moveX, 0]);
         }
     }
 
-    const startCardPositionCpt = useMemo(() => {
-        console.log(`${startDivPosition[0]} + ${movePosition[0]} = ${startDivPosition[0] + movePosition[0]}`)
-        return  `${startDivPosition[0] + movePosition[0]}px`;
-    }, [startDivPosition, movePosition])
+    const moveStyle = useMemo(() => {
+        let opacityTemp = 1
+        if (document.body) {
+            const {clientWidth} = document.body
+            Math.abs(movePosition[0]) >= (clientWidth / 1.5) ? setMovePosition([0, 0]) : null;
+            opacityTemp = (-1.5 / clientWidth) * Math.abs(movePosition[0]) + 1
+        }
 
 
-    useEffect(() => {
-        console.log('dragDiv', dragDiv)
-        const { offsetLeft, offsetTop } = dragDiv.current;
-        setStartDivPosition([offsetLeft, offsetTop])
-        currentLeft = offsetLeft
-    }, [])
+        const styles = {
+            transform: `translateX(${movePosition[0]}PX)`,
+            opacity: opacityTemp,
+            transition: 'all 0.1s',
+        }
+        return  styles;
+    }, [movePosition])
+
+
 
     useEffect(() => {
         dragDiv.current.addEventListener('touchstart', handleTouchStartFn);
         dragDiv.current.addEventListener('touchmove', handleTouchMoveFn);
+        dragDiv.current.addEventListener('touchend', handleTouchEndFn)
 
         return () => {
             dragDiv.current.removeEventListener('touchstart', handleTouchStartFn);
             dragDiv.current.removeEventListener('touchmove', handleTouchMoveFn);
+            dragDiv.current.removeEventListener('touchend', handleTouchEndFn);
         }
 
 
-    }, [startPositionX, movePosition])
+    }, [movePosition])
     return (
         <div className="card-day-plan">
-            <div className="day-item-one" ref={dragDiv} style={{left: `${startCardPositionCpt}`}}>
+            <div className="day-item-one" ref={dragDiv} style={moveStyle}>
                 { movePosition.join(',') }
             </div>
             {/*<div className="day-item-two">2</div>*/}
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -7,6 +7,8 @@ const baseConfig = require('./webpack.config.base.js');
 module.exports = merge(baseConfig, {
     mode: 'development',
     devServer: {
-        port: 8000
+        port: 8000, //设置端口
+        host : "0.0.0.0", //设置0.0.0.0使得可以通过本机ip访问项目
+        disableHostCheck : true
     }
 })
